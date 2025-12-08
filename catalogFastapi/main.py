3a0f0ecf423cbc81e747dea00390bd6d270dd258
Агадилов Алексей Владимирович <a.agadilov@gharysh.local>
2025-12-08 14:42:03 +0500

    working backend cloud percent

--- catalogFastapi/main.py
+++ catalogFastapi/main.py
@@ -4,11 +4,46 @@ import geopandas as gpd
 from shapely.geometry import Polygon
 import tempfile
 import os
+import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Geo Catalog Proxy with KML Filter")
 
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5501",
+        "http://127.0.0.1:5501",
+    ],
+    allow_origin_regex=r"http://10\.\d+\.\d+\.\d+:\d+",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 BASE_URL = "http://10.0.6.117:8001/CatalogService"
 
+
+
+df_clouds = pd.read_excel("ALL_2024.xlsx")
+
+
+# ✅ Приводим процент к float (21,8 → 21.8)
+df_clouds["Cloud_Coverage_%"] = (
+    df_clouds["Cloud_Coverage_%"]
+    .astype(str)
+    .str.replace(",", ".", regex=False)
+    .astype(float)
+)
+
+# ✅ Делаем словарь:
+# { Image_ID: процент }
+cloud_dict = dict(
+    zip(df_clouds["Image_ID"], df_clouds["Cloud_Coverage_%"])
+)
+# print(cloud_dict)
 
 # ===========================
 # ✅ ВСПОМОГАТЕЛЬНАЯ ФУНКЦИЯ
@@ -35,12 +70,13 @@ def polygon_from_coordinates_string(coord_str: str) -> Polygon:
 # ===========================
 @app.get("/catalog")
 async def get_catalog(
-    DateFr: str = Query(...),
-    DateTo: str = Query(...),
-    West: float = Query(...),
-    East: float = Query(...),
-    South: float = Query(...),
-    North: float = Query(...)
+    DateFr: str = Query("2024-10-30"),
+    DateTo: str = Query("2024-11-08"),
+    West: float = Query("74.459002"),
+    East: float = Query("53.499767"),
+    South: float = Query("49.275218"),
+    North: float = Query("65.710996"),
+    Cloud: float| None = Query(100)
 ):
     params = {
         "DateFr": DateFr,
@@ -50,11 +86,37 @@ async def get_catalog(
         "South": South,
         "North": North,
     }
+    print(Cloud)
 
     async with httpx.AsyncClient(timeout=60) as client:
         response = await client.get(BASE_URL, params=params)
+        data = response.json()
+    # print('work')
+    # return response.json()
+    filtered = []
 
-    return response.json()
+    for item in data["data"]:
+        print(item)
+        # ✅ Берём ID без .jpeg
+        image_id = item["new_quicklook"].replace(".jpeg", "")
+        print(image_id,"this is image id")
+        print(image_id,"image id")
+        # ✅ Получаем процент из Excel
+        cloud_value = cloud_dict.get(image_id)
+        print(cloud_value,"this cloud value")
+
+        # ✅ Если есть в Excel и проходит по лимиту
+        if cloud_value ==None :
+            item["Cloud_Coverage"] = 0  # ✅ добавляем поле
+            print(item,"item")
+            filtered.append(item)
+        elif cloud_value <= Cloud:
+            # cloud_value is not None and cloud_value <= Cloud:
+            item["Cloud_Coverage"] = cloud_value  # ✅ добавляем поле
+            print(item,"item")
+            filtered.append(item)
+    print(filtered,"this is filtered")
+    return {'data':filtered}
 
 
 # ===========================
--- js/service/catalogService.js
+++ js/service/catalogService.js
@@ -6,7 +6,8 @@ import { coordinatesFromKmlKmz, geoJson } from "../buttonEvents.js";
 const imageDataArray = []
 let foundImage;
 
-var path = "http://10.0.6.117:8001/CatalogService?DateFr=" 
+// var path = "http://10.0.6.117:8001/CatalogService?DateFr="
+var path = "http://127.0.0.1:9000/catalog?DateFr="
 //var pat2 = "http://127.0.0.1:3000/" 
 // var path = "http://old-eo.gharysh.kz/CatalogService?DateFr="
 var path2 = "http://10.0.11.107:3000/" 
